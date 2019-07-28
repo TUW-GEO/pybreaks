545d2e9b23cde1a3f9fe50d462ad78982129a53e
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2019-07-28 22:41:58 +0200

    Add some test data

---
+++ tests/test_break_detect.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Jul 28 19:18 2019
+
+@author: wolfgang
+"""
---
+++ tests/test_verify.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Jul 28 19:21 2019
+
+@author: wolfgang
+"""
