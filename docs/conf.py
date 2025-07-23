d7c9bb00551e2e4bc55a94b2d7a4cc49667e5935
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2025-07-23 17:12:37 +0200

    Update meta package

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "pybreaks"
-copyright = "2022, Wolfgang Preimesberger"
+copyright = "2025, Wolfgang Preimesberger"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
